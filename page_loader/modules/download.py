b700513a5f61f69f0cea2df221991e5fdc0dee88
Boris Ageyev <mr.broomy@gmail.com>
2022-05-25 16:40:09 +0500

    Under construction

--- page_loader/modules/download.py
+++ page_loader/modules/download.py
@@ -2,6 +2,7 @@ import os
 import requests
 import re
 import html
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 
@@ -17,7 +18,7 @@ def download(url, folder):
 
     print('tag_num:', len(img_tags))
     print('url:', url)
-    host_url = re.search('(?<=\://)(.*)(?=/)', url).group(1)
+    host_url = re.search('(?<=\://)(.*)(?=/|\Z)', url).group(1)
     
     #n = re.sub('(html?)$', '', url)
     #print('\n^htm:', n)
@@ -25,26 +26,36 @@ def download(url, folder):
     #print('\nfname:', file_name)
     file_path = os.path.join(folder, f'{host_url}.html')
     print('file_path:', file_path)
-    with open(file_path, "w") as f:
-        f.write(r.text)
+    #with open(file_path, "w") as f:
+    #    f.write(r.text)
     
 
     print('host_url:', host_url)
     for tag in img_tags:
         print(tag['src'])
         f_name, f_ext = os.path.splitext(tag['src'])
-        d = requests.get(os.path.join(url, tag['src']))
+        d = requests.get(urljoin(url, tag['src']))
         f_content = d.content
         print('cont:', len(f_content))
         print('orig:', f_name)
         #f_name_new = os.path.join(, f_name)
         
-        f_name_new = re.sub('\W+', '-', f_name)
-        print('fnn:', f_name_new)
-        f_path = os.path.join(f'{host_url}_files', f'{f_name_new}{f_ext}')
+        file = f_name.lstrip('/')
+        #print('fnn:', f_name_new)
+        
+        subst = re.sub('\W+', '-', host_url)
+        pure_path = f'{subst}_files'
+
+        subst = re.sub('\W+', '-', file)
+        pure_file = f'{subst}{f_ext}'
+
+        f_path = f'{pure_path}/{pure_file}'
         print('path_new:', f_path)
+        
+        # re.sub('\W+', '-',
+
         #new_image_url = os.path.join(host_url)
-        with open(file_path, "w") as f:
-        f.write(r.text)
+        #with open(file_path, "w") as f:
+        #    f.write(r.text)
 
     return file_path
